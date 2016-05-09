8865b82beace4abe73aafff6a32d8573809dcde8
CodeFuller <CodeFuller@gmail.com>
2016-05-09 12:21:06 +0300

    Replace stub data in AssemblyInfo.cs
    Fill AssemblyCompany with 'CodeFuller'
    Fill AssemblyDescription with assembly dll name
    Fill AssemblyVersion with '0.1.*'

--- CF.Extensions.Wpf/Properties/AssemblyInfo.cs
+++ CF.Extensions.Wpf/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CF.Extensions.Wpf")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("CF.Extensions.Wpf.dll")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CodeFuller")]
 [assembly: AssemblyProduct("CF.Extensions.Wpf")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
