7ce59db01b8efcdff984fbdd2cf439e221354ad5
CodeFuller <CodeFuller@gmail.com>
2016-05-09 17:26:07 +0300

    Add LegacyJsonMediaTypeConverterDelegatingHandler

---
+++ CF.Extensions/Json/LegacyJsonMediaTypeConverterDelegatingHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace CF.Extensions.Json
+{
+	/// <remarks>
+	/// http://byterot.blogspot.com.by/2012/06/asp-net-web-api-mediatypeformatter.html
+	/// </remarks>
+	public class LegacyJsonMediaTypeConverterDelegatingHandler : DelegatingHandler
+	{
+		protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			return base.SendAsync(request, cancellationToken)
+			 .ContinueWith(task =>
+			 {
+				 var httpResponseMessage = task.Result;
+				 if (httpResponseMessage.Content.Headers.ContentType.MediaType == "text/javascript")
+				 {
+					 httpResponseMessage.Content.Headers.ContentType.MediaType = "application/json";
+				 }
+				 return httpResponseMessage;
+			 }, cancellationToken);
+		}
+	}
+}
