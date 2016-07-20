c06a98d82fea473d03464069497f1de18673e4d0
CodeFuller <CodeFuller@gmail.com>
2016-07-20 23:04:55 +0300

    Add JsonObjectFormatter class

---
+++ CF.Extensions/Json/JsonObjectFormatter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace CF.Extensions.Json
+{
+	/// <summary>
+	/// Holder for ToJson() extension method
+	/// </summary>
+	public static class JsonObjectFormatter
+	{
+		/// <summary>
+		/// Is used for human-friendly dump of any object
+		/// </summary>
+		public static string ToJson(this object obj)
+		{
+			JavaScriptSerializer serializer = new JavaScriptSerializer();
+			return serializer.Serialize(obj);
+		}
+	}
+}
