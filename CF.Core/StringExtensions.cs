5a23db7fb5c192c06147cc70221262b7ac8b4bc4
CodeFuller <CodeFuller@gmail.com>
2016-07-31 10:47:10 +0300

    Add CF.Core.StringExtensions
    For this moment StringExtensions contains only one extension method -
    StringBuilder.AppendDelimited()

---
+++ CF.Core/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CF.Core
+{
+	/// <summary>
+	/// Holder for String and StringBuilder classes extension methods.
+	/// </summary>
+	public static class StringExtensions
+	{
+		/// <summary>
+		/// Appends strings to StringBuilder separating them with specified delimiter.
+		/// </summary>
+		public static void AppendDelimited(this StringBuilder sb, string value, string delimiter = ", ")
+		{
+			if (sb.Length > 0)
+			{
+				sb.Append(delimiter);
+			}
+			sb.Append(value);
+		}
+	}
+}
---
+++ CF.Library.Tests/CF.Core/StringExtensionsTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using CF.Core;
+using NUnit.Framework;
+
+namespace CF.Library.Tests.CF.Core
+{
+	[TestFixture]
+	public class StringExtensionsTests
+	{
+		[Test]
+		public void AppendDelimited_ForEmptyDestination_AddsStringWithoutDelimiter()
+		{
+			var sb = new StringBuilder();
+
+			sb.AppendDelimited("SomeString", "; ");
+
+			Assert.AreEqual(sb.ToString(), "SomeString");
+		}
+
+		[Test]
+		public void AppendDelimited_ForNonEmptyDestination_AddsStringWithDelimiter()
+		{
+			var sb = new StringBuilder();
+
+			sb.AppendDelimited("SomeString1", "; ");
+			sb.AppendDelimited("SomeString2", "; ");
+
+			Assert.AreEqual(sb.ToString(), "SomeString1; SomeString2");
+		}
+	}
+}
