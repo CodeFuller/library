43692a28f016057e7509c944a11e8e9d5825b723
CodeFuller <CodeFuller@gmail.com>
2016-08-22 11:07:29 +0300

    Add CF.Testing project

---
+++ CF.Testing/ConfigurationHelpers.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+
+namespace CF.Testing
+{
+	/// <summary>
+	/// Container for configuration helper methods.
+	/// </summary>
+	public static class ConfigurationHelpers
+	{
+		/// <summary>
+		/// Adds or updates specified configuration section.
+		/// </summary>
+		public static void SetConfigurationSection(string sectionName, ConfigurationSection section)
+		{
+			Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+			config.Sections.Remove(sectionName);
+			config.Sections.Add(sectionName, section);
+			config.Save(ConfigurationSaveMode.Modified);
+			ConfigurationManager.RefreshSection(sectionName);
+		}
+	}
+}
