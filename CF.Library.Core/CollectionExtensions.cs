048a625cf82d625e349a8df1f3a868ea8c08f2f2
CodeFuller <CodeFuller@gmail.com>
2016-08-27 10:22:29 +0300

    Cover CollectionExtensions in CF.Library.Core by tests

--- CF.Library.Core/CollectionExtensions.cs
+++ CF.Library.Core/CollectionExtensions.cs
@@ -9,26 +9,10 @@ namespace CF.Library.Core
 	public static class CollectionExtensions
 	{
 		/// <summary>
-		/// Adds default value for specific key to the dictionary if it doesn't exist
+		/// Returns value for specific key in the dictionary if it exists.
+		/// Otherwise returns default value of the type specified.
 		/// </summary>
-		public static void FillDefaultValue<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key) where TValue : new()
-		{
-			if (dict == null)
-			{
-				throw new ArgumentNullException(nameof(dict));
-			}
-
-			if (!dict.ContainsKey(key))
-			{
-				dict.Add(key, new TValue());
-			}
-		}
-
-		/// <summary>
-		/// Returns value for specific key in the dictionary if it exists
-		/// Otherwise returns default value of the type specified
-		/// </summary>
-		public static TValue SafeGet<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
+		public static TValue GetValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
 		{
 			if (dict == null)
 			{
@@ -41,8 +25,7 @@ namespace CF.Library.Core
 		}
 
 		/// <summary>
-		/// Returns value for specific key in the dictionary if it exists
-		/// Otherwise adds and returns default value of the type specified
+		/// Returns value for specific key in the dictionary. Default value is added for the key if required.
 		/// </summary>
 		public static TValue ProvideValue<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key) where TValue : new()
 		{
@@ -51,39 +34,13 @@ namespace CF.Library.Core
 				throw new ArgumentNullException(nameof(dict));
 			}
 
-			lock (dict)
+			TValue value;
+			if (!dict.TryGetValue(key, out value))
 			{
-				TValue value;
-				if (!dict.TryGetValue(key, out value))
-				{
-					value = new TValue();
-					dict.Add(key, value);
-				}
-				return value;
-			}
-		}
-
-		/// <summary>
-		/// Adds given value to the value stored at specified key in the dictionary
-		/// If value doesn't yet exist - just saves given value in the dictionary
-		/// </summary>
-		public static void SafeAdd<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key, TValue value)
-		{
-			if (dict == null)
-			{
-				throw new ArgumentNullException(nameof(dict));
-			}
-
-			if (dict.ContainsKey(key))
-			{
-				dynamic v1 = dict[key];
-				dynamic v2 = value;
-				dict[key] = v1 + v2;
-			}
-			else
-			{
+				value = new TValue();
 				dict.Add(key, value);
 			}
+			return value;
 		}
 	}
 }
---
+++ CF.Library.UnitTests/CF.Library.Core/CollectionExtensionsTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CF.Library.Core;
+using NUnit.Framework;
+
+namespace CF.Library.UnitTests.CF.Library.Core
+{
+	[TestFixture]
+	public class CollectionExtensionsTests
+	{
+		[Test]
+		public void GetValueOrDefault_WhenValueExistInDictionary_ReturnsExistingValue()
+		{
+			Dictionary<string, object> dict = new Dictionary<string, object>();
+			object existingValue = new object();
+			dict.Add("key", existingValue);
+
+			object returnedValue = dict.GetValueOrDefault("key");
+
+			Assert.AreSame(existingValue, returnedValue);
+		}
+
+		[Test]
+		public void GetValueOrDefault_WhenValueDoesNotExistInDictionary_ReturnsDefaultTypeValue()
+		{
+			Dictionary<string, object> dict = new Dictionary<string, object>();
+
+			object returnedValue = dict.GetValueOrDefault("key");
+
+			Assert.IsTrue(EqualityComparer<object>.Default.Equals(returnedValue, default(object)));
+		}
+
+		[Test]
+		public void GetValueOrDefault_WhenDictArgumentIsNull_ThrowsArgumentNullException()
+		{
+			Assert.Throws<ArgumentNullException>(() => ((Dictionary<string, object>)null).GetValueOrDefault("key"));
+		}
+
+		[Test]
+		public void ProvideValue_WhenValueExistInDictionary_DoesNotAffectDictionary()
+		{
+			Dictionary<string, object> dict = new Dictionary<string, object>();
+			object existingValue = new object();
+			dict.Add("key", existingValue);
+
+			dict.ProvideValue("key");
+
+			Assert.AreEqual(1, dict.Count);
+			Assert.AreSame(existingValue, dict["key"]);
+		}
+
+		[Test]
+		public void ProvideValue_WhenValueExistInDictionary_ReturnsExistingValue()
+		{
+			Dictionary<string, object> dict = new Dictionary<string, object>();
+			object existingValue = new object();
+			dict.Add("key", existingValue);
+
+			object returnedValue = dict.ProvideValue("key");
+
+			Assert.AreSame(returnedValue, existingValue);
+		}
+
+		[Test]
+		public void ProvideValue_WhenValueDoesNotExistInDictionary_AddsValueToDictionary()
+		{
+			Dictionary<string, object> dict = new Dictionary<string, object>();
+
+			dict.ProvideValue("key");
+
+			Assert.AreEqual(1, dict.Count);
+			Assert.IsTrue(dict.ContainsKey("key"));
+		}
+
+		[Test]
+		public void ProvideValue_WhenValueDoesNotExistInDictionary_ReturnsAddedValue()
+		{
+			Dictionary<string, object> dict = new Dictionary<string, object>();
+
+			object returnedValue = dict.ProvideValue("key");
+
+			Assert.AreSame(dict["key"], returnedValue);
+		}
+
+		[Test]
+		public void ProvideValue_WhenDictArgumentIsNull_ThrowsArgumentNullException()
+		{
+			Assert.Throws<ArgumentNullException>(() => ((Dictionary<string, object>)null).ProvideValue("key"));
+		}
+	}
+}
