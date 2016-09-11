f79a6195423e818e2503ef84e846100de96d3672
CodeFuller <CodeFuller@gmail.com>
2016-09-11 15:30:20 +0300

    Add IInstanceCreator interface for DI usage

---
+++ CF.Library.Core/DI/IInstanceCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CF.Library.Core.DI
+{
+	/// <summary>
+	/// Interface for instance creation.
+	/// </summary>
+	public interface IInstanceCreator
+	{
+		/// <summary>
+		/// Creates instance of specified type, passing given arguments to its constructor.
+		/// </summary>
+		object CreateInstance(Type type, params ConstructorArgument[] constructorArguments);
+	}
+}
