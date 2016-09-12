2d7649b5bf695b6bcf0a9360627283c489332e1f
CodeFuller <CodeFuller@gmail.com>
2016-09-12 20:50:49 +0300

    Add IBootstrapper interface

---
+++ CF.Library.Core/Interfaces/IBootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CF.Library.Core.Interfaces
+{
+	/// <summary>
+	/// Interface for application bootstrapper.
+	/// </summary>
+	public interface IBootstrapper : IDisposable
+	{
+		/// <summary>
+		/// Bootstraps application.
+		/// </summary>
+		void Run();
+	}
+}
