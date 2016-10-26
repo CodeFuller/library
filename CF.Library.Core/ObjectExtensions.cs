9eae85e28073b2fc05a23a33cd59427489ba213a
CodeFuller <CodeFuller@gmail.com>
2016-10-26 21:48:49 +0300

    Add CF.Library.Core.DataReaderExtensions class

---
+++ CF.Library.Core/ObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CF.Library.Core
+{
+	/// <summary>
+	/// Holder for Object extension methods.
+	/// </summary>
+	public static class ObjectExtensions
+	{
+		/// <summary>
+		/// Safe ToString() that correctly handles null objects.
+		/// </summary>
+		public static string Stringify(this Uri uri)
+		{
+			return uri?.ToString() ?? "<null>";
+		}
+	}
+}
