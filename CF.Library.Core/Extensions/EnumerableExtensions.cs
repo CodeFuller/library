c75d8fa01c0eaac7a6f08dfce9a3fc22a7471c05
CodeFuller <CodeFuller@gmail.com>
2016-11-04 08:33:21 +0300

    Add CF.Library.Core.Extensions.DataReaderExtensions class

---
+++ CF.Library.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace CF.Library.Core.Extensions
+{
+	/// <summary>
+	/// Holder for IEnumerable extension methods
+	/// </summary>
+	public static class EnumerableExtensions
+	{
+		/// <summary>
+		/// Creates collection from IEnumerable.
+		/// </summary>
+		public static Collection<T> ToCollection<T>(this IEnumerable<T> source)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+			return new Collection<T>(source.ToList());
+		}
+	}
+}
