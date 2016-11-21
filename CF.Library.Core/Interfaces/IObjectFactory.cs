c3d0ded037c45627ed5c59b4e6b8460dc2d8848c
CodeFuller <CodeFuller@gmail.com>
2016-11-21 21:57:48 +0300

    Add CF.Library.Unity.UnityBasedObjectFactory class

---
+++ CF.Library.Core/Interfaces/IObjectFactory.cs
@@ -0,0 +1,13 @@
+﻿namespace CF.Library.Core.Interfaces
+{
+	/// <summary>
+	/// Interface for Object Factory.
+	/// </summary>
+	public interface IObjectFactory<out TType> where TType : class
+	{
+		/// <summary>
+		/// Creates instance of TType.
+		/// </summary>
+		TType CreateInstance();
+	}
+}
---
+++ CF.Library.Unity/UnityBasedObjectFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CF.Library.Core.Interfaces;
+using Microsoft.Practices.Unity;
+
+namespace CF.Library.Unity
+{
+	/// <summary>
+	/// Implementation of IObjectFactory based on Unity container.
+	/// </summary>
+	public class UnityBasedObjectFactory<TType> : IObjectFactory<TType> where TType : class
+	{
+		private readonly IUnityContainer unityContainer;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public UnityBasedObjectFactory(IUnityContainer unityContainer)
+		{
+			if (unityContainer == null)
+			{
+				throw new ArgumentNullException(nameof(unityContainer));
+			}
+
+			this.unityContainer = unityContainer;
+		}
+
+		/// <summary>
+		/// Creates object instance by resolving type from Unity container.
+		/// </summary>
+		/// <returns></returns>
+		public TType CreateInstance()
+		{
+			return unityContainer.Resolve<TType>();
+		}
+	}
+}
