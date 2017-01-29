2fd4c94692e5beae681e2006a0eb62b4239f927d
CodeFuller <CodeFuller@gmail.com>
2017-01-29 15:31:12 +0300

    Add CF.Library.Patterns.ProcessStateManager class

---
+++ CF.Library.Patterns/IProcessStateManager.cs
@@ -0,0 +1,24 @@
+﻿namespace CF.Library.Patterns
+{
+	/// <summary>
+	/// Interface for managing of process state and switching between states, e.g. stopped, running, paused.
+	/// </summary>
+	public interface IProcessStateManager
+	{
+		/// <summary>
+		/// Switches process to Running state.
+		/// </summary>
+		void Start();
+
+		/// <summary>
+		/// Switches process to Stopped state.
+		/// </summary>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1716:IdentifiersShouldNotMatchKeywords", Justification = "Stop name is the best in current semantics.")]
+		void Stop();
+
+		/// <summary>
+		/// Switches process to Paused state.
+		/// </summary>
+		void Pause();
+	}
+}
---
+++ CF.Library.Patterns/ProcessStateManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using static System.FormattableString;
+
+namespace CF.Library.Patterns
+{
+	/// <summary>
+	/// Possible values of process state.
+	/// </summary>
+	public enum ProcessState
+	{
+		/// <summary>
+		/// Process is in undefined state.
+		/// </summary>
+		Undefined,
+
+		/// <summary>
+		/// Process is in stopped state.
+		/// </summary>
+		Stopped,
+
+		/// <summary>
+		/// Process is in running state.
+		/// </summary>
+		Running,
+
+		/// <summary>
+		/// Process is in paused state.
+		/// </summary>
+		Paused,
+	}
+
+	/// <summary>
+	/// Manages process state and switching between states, e.g. stopped, running, paused.
+	/// </summary>
+	public class ProcessStateManager : IProcessStateManager
+	{
+		private ProcessState state;
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		public ProcessStateManager()
+		{
+			state = ProcessState.Stopped;
+		}
+
+		/// <summary>
+		/// Switches process to Running state.
+		/// </summary>
+		public void Start()
+		{
+			if (state != ProcessState.Stopped && state != ProcessState.Paused)
+			{
+				throw new InvalidOperationException(Invariant($"Could not start while in '{state}' state"));
+			}
+
+			state = ProcessState.Running;
+		}
+
+		/// <summary>
+		/// Switches process to Stopped state.
+		/// </summary>
+		public void Stop()
+		{
+			if (state != ProcessState.Running)
+			{
+				throw new InvalidOperationException(Invariant($"Could not stop while in '{state}' state"));
+			}
+
+			state = ProcessState.Stopped;
+		}
+
+		/// <summary>
+		/// Switches process to Paused state.
+		/// </summary>
+		public void Pause()
+		{
+			if (state != ProcessState.Running)
+			{
+				throw new InvalidOperationException(Invariant($"Could not pause while in '{state}' state"));
+			}
+
+			state = ProcessState.Paused;
+		}
+	}
+}
