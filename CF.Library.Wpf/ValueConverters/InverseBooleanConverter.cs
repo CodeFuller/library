6accedcb90aa10193b886f7c5dfb8d9d1bff33c1
CodeFuller <CodeFuller@gmail.com>
2017-04-16 22:42:05 +0300

    Add CF.Library.Wpf.ValueConverters.InverseBooleanConverter class

---
+++ CF.Library.Wpf/ValueConverters/InverseBooleanConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CF.Library.Wpf.ValueConverters
+{
+	/// <remarks>
+	/// Copy/paste from http://stackoverflow.com/a/1039681/5740031
+	/// </remarks>>
+	[ValueConversion(typeof(bool), typeof(bool))]
+	public class InverseBooleanConverter : IValueConverter
+	{
+		/// <summary>
+		/// Converts value.
+		/// </summary>
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (targetType != typeof(bool))
+			{
+				throw new InvalidOperationException("The target must be a boolean");
+			}
+
+			return !(bool)value;
+		}
+
+		/// <summary>
+		/// ConvertBack() method is not implemented for this converter.
+		/// </summary>
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
