0ce551f576d56195b613014633997cc44f76cad5
CodeFuller <CodeFuller@gmail.com>
2017-09-20 09:39:29 +0300

    Add CF.Library.Wpf.Extensions.DependencyObjectExtensions class

---
+++ CF.Library.Wpf/Extensions/DependencyObjectExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CF.Library.Wpf.Extensions
+{
+	/// <summary>
+	/// Holder for DependencyObject extension methods.
+	/// </summary>
+	public static class DependencyObjectExtensions
+	{
+		/// <summary>
+		/// Checks whether any of child controls have validation errors.
+		/// </summary>
+		/// <remarks>
+		/// https://stackoverflow.com/a/4650392/5740031
+		/// </remarks>
+		public static bool IsValid(this DependencyObject obj)
+		{
+			return !Validation.GetHasError(obj) &&
+			LogicalTreeHelper.GetChildren(obj)
+			.OfType<DependencyObject>()
+			.All(IsValid);
+		}
+	}
+}
