c47e9b788855353125ec8eee893acb5f778d9c5b
CodeFuller <CodeFuller@gmail.com>
2017-09-21 08:17:43 +0300

    Add CF.Library.WpfWindowService class

---
+++ CF.Library.Core/Enums/ShowMessageBoxIcon.cs
@@ -0,0 +1,43 @@
+﻿namespace CF.Library.Core.Enums
+{
+	/// <summary>
+	/// Enumeration of possible icons that are displayed in MessageBox.
+	/// </summary>
+	public enum ShowMessageBoxIcon
+	{
+		/// <summary>
+		/// Undefined default value.
+		/// </summary>
+		None,
+
+		/// <summary>
+		/// No icon is displayed.
+		/// </summary>
+		NoImage,
+
+		/// <summary>
+		/// Information icon is displayed.
+		/// </summary>
+		Information,
+
+		/// <summary>
+		/// Warning icon is displayed.
+		/// </summary>
+		Warning,
+
+		/// <summary>
+		/// Error icon is displayed.
+		/// </summary>
+		Error,
+
+		/// <summary>
+		/// Question icon is displayed.
+		/// </summary>
+		Question,
+
+		/// <summary>
+		/// Exclamation icon is displayed.
+		/// </summary>
+		Exclamation,
+	}
+}
---
+++ CF.Library.Core/Enums/ShowMessageBoxResult.cs
@@ -0,0 +1,38 @@
+﻿namespace CF.Library.Core.Enums
+{
+	/// <summary>
+	/// Enumeration of possible outcomes of showing message box.
+	/// </summary>
+	public enum ShowMessageBoxResult
+	{
+		/// <summary>
+		/// Default undefined value.
+		/// </summary>
+		None,
+
+		/// <summary>
+		/// Message box returned no result.
+		/// </summary>
+		NoResult,
+
+		/// <summary>
+		/// User pressed OK button.
+		/// </summary>
+		Ok,
+
+		/// <summary>
+		/// User pressed Cancel button.
+		/// </summary>
+		Cancel,
+
+		/// <summary>
+		/// User pressed Yes button.
+		/// </summary>
+		Yes,
+
+		/// <summary>
+		/// User pressed No button.
+		/// </summary>
+		No,
+	}
+}
