d7c0c01009d0bc0962e1cab00522468586d549c9
CodeFuller <CodeFuller@gmail.com>
2017-09-30 14:46:15 +0300

    Add EnumerableExtensions.UniqueOrDefault() method

--- CF.Library.Core/Extensions/EnumerableExtensions.cs
+++ CF.Library.Core/Extensions/EnumerableExtensions.cs
@@ -21,5 +21,19 @@ namespace CF.Library.Core.Extensions
 			}
 			return new Collection<T>(source.ToList());
 		}
+
+		/// <summary>
+		/// Creates collection from IEnumerable.
+		/// </summary>
+		public static T UniqueOrDefault<T>(this IEnumerable<T> source)
+		{
+			if (source == null)
+			{
+				throw new ArgumentNullException(nameof(source));
+			}
+
+			var distinctValues = source.Distinct().ToList();
+			return distinctValues.Count == 1 ? distinctValues[0] : default(T);
+		}
 	}
 }
