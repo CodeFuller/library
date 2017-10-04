2a40a9a85e1aaa09412c2351714bb8076a0b40a1
CodeFuller <CodeFuller@gmail.com>
2017-10-04 07:53:31 +0300

    Add ObservableCollectionExtensions.AddRange() method

---
+++ CF.Library.Core/Extensions/ObservableCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace CF.Library.Core.Extensions
+{
+	/// <summary>
+	/// Holder for ObservableCollection extension methods.
+	/// </summary>
+	public static class ObservableCollectionExtensions
+	{
+		/// <summary>
+		/// Adds new items to the end of ObservableCollection.
+		/// </summary>
+		public static void AddRange<T>(this ObservableCollection<T> collection, IEnumerable<T> newItems)
+		{
+			foreach (var item in newItems)
+			{
+				collection.Add(item);
+			}
+		}
+	}
+}
