710e03875bf534138d0fd00a5c6ba0a9b78356bc
CodeFuller <CodeFuller@gmail.com>
2017-12-16 19:39:17 +0300

    Add CF.Library.Core.Facades.DateTimeOffsetFacade class

---
+++ CF.Library.Core/Facades/DateTimeOffsetFacade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CF.Library.Core.Facades
+{
+	public class DateTimeOffsetFacade : IDateTimeOffsetFacade
+	{
+		public DateTimeOffset Now => DateTimeOffset.Now;
+
+		public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+	}
+}
---
+++ CF.Library.Core/Facades/IDateTimeOffsetFacade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CF.Library.Core.Facades
+{
+	public interface IDateTimeOffsetFacade
+	{
+		DateTimeOffset Now { get; }
+
+		DateTimeOffset UtcNow { get; }
+	}
+}
