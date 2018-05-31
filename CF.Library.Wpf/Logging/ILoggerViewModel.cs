bae91d248546ee2d135fa7e3cddcff712ba01312
CodeFuller <CodeFuller@gmail.com>
2018-05-31 21:27:18 +0300

    Add LoggerViewModel

---
+++ CF.Library.Wpf/Logging/ILoggerViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace CF.Library.Wpf.Logging
+{
+	public interface ILoggerViewModel
+	{
+		ObservableCollection<LogMessage> Messages { get; }
+	}
+}
---
+++ CF.Library.Wpf/Logging/LogMessage.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using CF.Library.Core.Exceptions;
+using CF.Library.Core.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace CF.Library.Wpf.Logging
+{
+	public class LogMessage
+	{
+		public LogLevel Level { get; set; }
+
+		public string Message { get; set; }
+
+		public LogMessage(LogLevel level, string message)
+		{
+			Level = level;
+			Message = FormattableStringExtensions.Current($"{DateTime.Now:yyyy-MM-dd  HH:mm:ss}  {GetLevelString(level)}  {message}");
+		}
+
+		private static string GetLevelString(LogLevel level)
+		{
+			switch (level)
+			{
+				case LogLevel.Critical:
+					return "CRITICAL:";
+
+				case LogLevel.Error:
+					return "ERROR:  ";
+
+				case LogLevel.Warning:
+					return "WARNING:";
+
+				case LogLevel.Information:
+					return "INFO:   ";
+
+				case LogLevel.Debug:
+					return "DEBUG:  ";
+
+				case LogLevel.Trace:
+					return "TRACE:  ";
+
+				default:
+					throw new UnexpectedEnumValueException(level);
+			}
+		}
+	}
+}
