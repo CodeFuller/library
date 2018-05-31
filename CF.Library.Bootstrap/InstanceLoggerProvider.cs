c90ba32a5aca14150f0b0d0f9cafcd5879963a78
CodeFuller <CodeFuller@gmail.com>
2018-05-31 22:18:29 +0300

    Move InstanceLoggerProvider to CF.Library.Bootstrap
    Not all consumers of InstanceLoggerProvider class will require CF.Library.Logging dependency.

--- CF.Library.Bootstrap/InstanceLoggerProvider.cs
+++ CF.Library.Bootstrap/InstanceLoggerProvider.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.Extensions.Logging;
 
-namespace CF.Library.Logging
+namespace CF.Library.Bootstrap
 {
 	public sealed class InstanceLoggerProvider : ILoggerProvider
 	{
