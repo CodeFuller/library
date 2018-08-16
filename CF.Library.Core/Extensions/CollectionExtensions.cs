db67db4d6dd90ecf671c338bc787baf2658b0437
CodeFuller <CodeFuller@gmail.com>
2018-08-16 08:17:08 +0300

    Reorganize collection extension methods
    1. Collection type in AddRange() was changed from ObservableCollection<T> to ICollection<T>
    2. ProvideValue() for Dictonary was removed, because it already exists in System.Collections.Generic.CollectionExtensions

--- CF.Library.Core/Extensions/CollectionExtensions.cs
+++ CF.Library.Core/Extensions/CollectionExtensions.cs
@@ -1,46 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace CF.Library.Core.Extensions
 {
-	/// <summary>
-	/// Holder for collection extension methods
-	/// </summary>
 	public static class CollectionExtensions
 	{
-		/// <summary>
-		/// Returns value for specific key in the dictionary if it exists.
-		/// Otherwise returns default value of the type specified.
-		/// </summary>
-		public static TValue GetValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
+		public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> newItems)
 		{
-			if (dict == null)
+			foreach (var item in newItems)
 			{
-				throw new ArgumentNullException(nameof(dict));
+				collection.Add(item);
 			}
-
-			TValue value;
-			dict.TryGetValue(key, out value);
-			return value;
-		}
-
-		/// <summary>
-		/// Returns value for specific key in the dictionary. Default value is added for the key if required.
-		/// </summary>
-		public static TValue ProvideValue<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key) where TValue : new()
-		{
-			if (dict == null)
-			{
-				throw new ArgumentNullException(nameof(dict));
-			}
-
-			TValue value;
-			if (!dict.TryGetValue(key, out value))
-			{
-				value = new TValue();
-				dict.Add(key, value);
-			}
-			return value;
 		}
 	}
 }
