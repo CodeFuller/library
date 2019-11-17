fa3524d565d99e539a230844279896840d46ccec
CodeFuller <CodeFuller@gmail.com>
2019-11-17 22:19:38 +0300

    Reorganize solution structure

---
+++ src/CF.Library.Bootstrap/ConsoleApplication.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace CF.Library.Bootstrap
+{
+	public class ConsoleApplication
+	{
+		private readonly BasicApplicationBootstrapper<IApplicationLogic> bootstrapper;
+
+		public ConsoleApplication(BasicApplicationBootstrapper<IApplicationLogic> bootstrapper)
+		{
+			this.bootstrapper = bootstrapper ?? throw new ArgumentNullException(nameof(bootstrapper));
+		}
+
+		public async Task<int> Run(string[] args)
+		{
+			ILogger<ConsoleApplication> logger = null;
+
+			try
+			{
+				var application = bootstrapper.Bootstrap(args);
+				logger = bootstrapper.GetLogger<ConsoleApplication>();
+
+				CancellationTokenSource cts = new CancellationTokenSource();
+
+				Console.CancelKeyPress += delegate
+				{
+					logger.LogInformation("CTRL + C is pressed");
+					cts.Cancel();
+				};
+
+				return await application.Run(args, cts.Token);
+			}
+			catch (OperationCanceledException)
+			{
+				logger?.LogInformation("The program execution was aborted");
+				return 1;
+			}
+			catch (Exception e)
+			{
+				if (logger != null)
+				{
+					logger.LogCritical(e, "Exception caught");
+				}
+				else
+				{
+					Console.Error.WriteLine(e);
+				}
+				return 1;
+			}
+		}
+	}
+}
---
+++ src/CF.Library.Configuration/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Microsoft.Extensions.Configuration;
+
+namespace CF.Library.Configuration
+{
+	public static class ConfigurationBuilderExtensions
+	{
+		private const string DefaultSettingsFileName = "AppSettings.json";
+
+		public static IConfigurationBuilder LoadSettings(this IConfigurationBuilder configurationBuilder, string[] args)
+		{
+			return configurationBuilder.LoadSettings(DefaultSettingsFileName, true, args);
+		}
+
+		public static IConfigurationBuilder LoadSettings(this IConfigurationBuilder configurationBuilder, string configFileName, string[] args)
+		{
+			return configurationBuilder.LoadSettings(configFileName, false, args);
+		}
+
+		private static IConfigurationBuilder LoadSettings(this IConfigurationBuilder configurationBuilder,
+			string configFileName, bool configFileIsOptional, string[] args)
+		{
+			configurationBuilder.AddJsonFile(configFileName, configFileIsOptional);
+
+			args = AddConfigFilesFromCommandLine(configurationBuilder, args);
+			return configurationBuilder.AddEnvironmentVariables()
+				.AddCommandLine(args);
+		}
+
+		private static string[] AddConfigFilesFromCommandLine(IConfigurationBuilder configurationBuilder, string[] args)
+		{
+			var configFileKeyRegex = new Regex("^--config=(.+)$");
+
+			var restArgs = new List<string>();
+			foreach (var arg in args)
+			{
+				var match = configFileKeyRegex.Match(arg);
+				if (match.Success)
+				{
+					configurationBuilder.AddJsonFile(match.Groups[1].Value, false);
+				}
+				else
+				{
+					restArgs.Add(arg);
+				}
+			}
+
+			return restArgs.ToArray();
+		}
+	}
+}
