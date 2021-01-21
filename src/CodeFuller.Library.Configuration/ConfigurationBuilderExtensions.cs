06a830afaf70baec79edaebbf612d72f83c093ac
CodeFuller <CodeFuller@gmail.com>
2021-01-21 22:47:32 +0300

    Refine CodeFuller.Library.Configuration package

--- src/CodeFuller.Library.Configuration/ConfigurationBuilderExtensions.cs
+++ src/CodeFuller.Library.Configuration/ConfigurationBuilderExtensions.cs
@@ -9,11 +9,20 @@ namespace CodeFuller.Library.Configuration
 {
 	public static class ConfigurationBuilderExtensions
 	{
-		public static IConfigurationBuilder LoadSettings(this IConfigurationBuilder configurationBuilder, string configDirectoryName, string[] args)
+		/// <summary>
+		/// Adds configuration sources in the following order:
+		/// <list type="number">
+		/// <item><description>JSON files from provided config directory path (files are enumerated by *.json search pattern).</description></item>
+		/// <item><description>JSON files specified as command line arguments (--config=path1 --config=path2 ...).</description></item>
+		/// <item><description>Environment variables.</description></item>
+		/// <item><description>Command line arguments (excluding --config values).</description></item>
+		/// </list>
+		/// </summary>
+		public static IConfigurationBuilder LoadSettings(this IConfigurationBuilder configurationBuilder, string configDirectoryPath, string[] commandLineArgs)
 		{
-			AddJsonConfigs(configurationBuilder, configDirectoryName);
+			AddJsonConfigs(configurationBuilder, configDirectoryPath);
 
-			var restArgs = AddConfigFilesFromCommandLine(configurationBuilder, args);
+			var restArgs = AddConfigFilesFromCommandLine(configurationBuilder, commandLineArgs);
 
 			return configurationBuilder
 				.AddEnvironmentVariables()
@@ -25,34 +34,34 @@ namespace CodeFuller.Library.Configuration
 			var exeDirectoryPath = Path.GetDirectoryName(AppContext.BaseDirectory);
 
 			var confDir = Path.Combine(exeDirectoryPath ?? String.Empty, configDirectoryName);
-			var jsonConfigFiles = Directory.EnumerateFiles(confDir, "*.json*", SearchOption.TopDirectoryOnly)
+			var jsonConfigFiles = Directory.EnumerateFiles(confDir, "*.json", SearchOption.TopDirectoryOnly)
 				.OrderBy(x => x);
 
 			foreach (var configFile in jsonConfigFiles)
 			{
-				configurationBuilder.AddJsonFile(configFile, false);
+				configurationBuilder.AddJsonFile(configFile, optional: false);
 			}
 		}
 
 		private static string[] AddConfigFilesFromCommandLine(IConfigurationBuilder configurationBuilder, string[] args)
 		{
-			var configFileKeyRegex = new Regex("^--config=(.+)$");
+			var configFileKeyRegex = new Regex("^--config=(.+)$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-			var restArgs = new List<string>();
+			var remainingArgs = new List<string>();
 			foreach (var arg in args)
 			{
 				var match = configFileKeyRegex.Match(arg);
 				if (match.Success)
 				{
-					configurationBuilder.AddJsonFile(match.Groups[1].Value, false);
+					configurationBuilder.AddJsonFile(match.Groups[1].Value, optional: false);
 				}
 				else
 				{
-					restArgs.Add(arg);
+					remainingArgs.Add(arg);
 				}
 			}
 
-			return restArgs.ToArray();
+			return remainingArgs.ToArray();
 		}
 	}
 }
