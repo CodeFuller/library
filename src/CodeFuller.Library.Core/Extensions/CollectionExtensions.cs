4f8ccb50fd13a700fd723668335729d387ed124a
CodeFuller <CodeFuller@gmail.com>
2021-01-22 07:00:05 +0300

    Add XML commentary for ICollection.AddRange extension method

--- src/CodeFuller.Library.Core/Extensions/CollectionExtensions.cs
+++ src/CodeFuller.Library.Core/Extensions/CollectionExtensions.cs
@@ -4,6 +4,9 @@ namespace CodeFuller.Library.Core.Extensions
 {
 	public static class CollectionExtensions
 	{
+		/// <summary>
+		/// Adds the elements to the end of the collection.
+		/// </summary>
 		public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> newItems)
 		{
 			foreach (var item in newItems)
