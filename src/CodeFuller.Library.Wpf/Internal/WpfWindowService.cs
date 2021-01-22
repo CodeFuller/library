8ad7335730d1f6c3514f88f0226b4cea8a206db5
CodeFuller <CodeFuller@gmail.com>
2021-01-22 20:20:16 +0300

    Make WpfWindowService class internal

---
+++ src/CodeFuller.Library.Wpf/Internal/WpfWindowService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Windows;
+using CodeFuller.Library.Wpf.Interfaces;
+
+namespace CodeFuller.Library.Wpf.Internal
+{
+	internal class WpfWindowService : IWindowService
+	{
+		public ShowMessageBoxResult ShowMessageBox(string message)
+		{
+			var result = MessageBox.Show(message);
+			return ConvertShowMessageBoxResult(result);
+		}
+
+		public ShowMessageBoxResult ShowMessageBox(string message, string title, ShowMessageBoxButton button, ShowMessageBoxIcon icon)
+		{
+			var result = MessageBox.Show(message, title, ConvertShowMessageBoxButton(button), ConvertShowMessageBoxIcon(icon));
+			return ConvertShowMessageBoxResult(result);
+		}
+
+		private static MessageBoxButton ConvertShowMessageBoxButton(ShowMessageBoxButton button)
+		{
+			return button switch
+			{
+				ShowMessageBoxButton.Ok => MessageBoxButton.OK,
+				ShowMessageBoxButton.OkCancel => MessageBoxButton.OKCancel,
+				ShowMessageBoxButton.YesNoCancel => MessageBoxButton.YesNoCancel,
+				ShowMessageBoxButton.YesNo => MessageBoxButton.YesNo,
+				_ => throw new NotSupportedException($"The MessageBox button type {button} is not supported")
+			};
+		}
+
+		private static MessageBoxImage ConvertShowMessageBoxIcon(ShowMessageBoxIcon icon)
+		{
+			return icon switch
+			{
+				ShowMessageBoxIcon.NoImage => MessageBoxImage.None,
+				ShowMessageBoxIcon.Information => MessageBoxImage.Information,
+				ShowMessageBoxIcon.Warning => MessageBoxImage.Warning,
+				ShowMessageBoxIcon.Error => MessageBoxImage.Error,
+				ShowMessageBoxIcon.Question => MessageBoxImage.Question,
+				ShowMessageBoxIcon.Exclamation => MessageBoxImage.Exclamation,
+				_ => throw new NotSupportedException($"The MessageBox icon type {icon} is not supported")
+			};
+		}
+
+		private static ShowMessageBoxResult ConvertShowMessageBoxResult(MessageBoxResult result)
+		{
+			return result switch
+			{
+				MessageBoxResult.None => ShowMessageBoxResult.NoResult,
+				MessageBoxResult.OK => ShowMessageBoxResult.Ok,
+				MessageBoxResult.Cancel => ShowMessageBoxResult.Cancel,
+				MessageBoxResult.Yes => ShowMessageBoxResult.Yes,
+				MessageBoxResult.No => ShowMessageBoxResult.No,
+				_ => throw new NotSupportedException($"The MessageBox result type {result} is not supported")
+			};
+		}
+	}
+}
---
+++ src/CodeFuller.Library.Wpf/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using CodeFuller.Library.Wpf.Interfaces;
+using CodeFuller.Library.Wpf.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeFuller.Library.Wpf
+{
+	/// <summary>
+	/// Extension methods for adding WPF window service to the DI container.
+	/// </summary>
+	public static class ServiceCollectionExtensions
+	{
+		/// <summary>
+		/// Adds services required for using window service.
+		/// </summary>
+		/// <param name="services">The <see cref="IServiceCollection"/> to add the services to.</param>
+		/// <returns>The <see cref="IServiceCollection"/> so that additional calls can be chained.</returns>
+		public static IServiceCollection AddWpfWindowService(this IServiceCollection services)
+		{
+			services.AddSingleton<IWindowService, WpfWindowService>();
+
+			return services;
+		}
+	}
+}
