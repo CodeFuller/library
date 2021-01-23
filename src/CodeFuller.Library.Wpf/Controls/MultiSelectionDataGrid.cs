8370f1d5cef83aa1eb68a2116852050e32a7e32a
CodeFuller <CodeFuller@gmail.com>
2021-01-23 16:40:26 +0300

    Add MultiSelectionDataGrid control to CodeFuller.Library.Wpf package

---
+++ src/CodeFuller.Library.Wpf/Controls/MultiSelectionDataGrid.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CodeFuller.Library.Wpf.Controls
+{
+	/// <summary>
+	/// Custom <see cref="DataGrid"/> with support of multi-select.
+	/// </summary>
+	// https://stackoverflow.com/a/22908694/5740031
+	public class MultiSelectionDataGrid : DataGrid
+	{
+		/// <summary>
+		/// Dependency property for <see cref="SelectedItemsList"/>.
+		/// </summary>
+		public static readonly DependencyProperty SelectedItemsListProperty =
+			DependencyProperty.Register(nameof(SelectedItemsList), typeof(IList), typeof(MultiSelectionDataGrid), new PropertyMetadata(null));
+
+		/// <summary>
+		/// Gets or sets the list of selected items.
+		/// </summary>
+#pragma warning disable CA2227 // Collection properties should be read only
+		public IList SelectedItemsList
+#pragma warning restore CA2227 // Collection properties should be read only
+		{
+			get => (IList)GetValue(SelectedItemsListProperty);
+			set => SetValue(SelectedItemsListProperty, value);
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MultiSelectionDataGrid"/> class.
+		/// </summary>
+		public MultiSelectionDataGrid()
+		{
+			SelectionChanged += CustomDataGrid_SelectionChanged;
+			KeyUp += DataGrid_KeyUp;
+		}
+
+		private void CustomDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			SelectedItemsList = SelectedItems;
+		}
+
+		private static void DataGrid_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Escape)
+			{
+				(sender as DataGrid)?.UnselectAll();
+			}
+		}
+	}
+}
