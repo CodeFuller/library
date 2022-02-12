a77c46eefd6c6a9572c42e0d7404ec4ea72f5cfe
CodeFuller <CodeFuller@gmail.com>
2022-02-12 13:59:04 +0300

    Add GetViewModel extension method

---
+++ src/CodeFuller.Library.Wpf/Extensions/ObjectExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CodeFuller.Library.Wpf.Extensions
+{
+	/// <summary>
+	/// Extension methods for generic objects.
+	/// </summary>
+	public static class ObjectExtensions
+	{
+		/// <summary>
+		/// Gets strongly-typed ViewModel from the DataContext object.
+		/// </summary>
+		/// <typeparam name="TViewModel">The type of expected ViewModel.</typeparam>
+		/// <param name="dataContext">DataContext object.</param>
+		/// <returns>Strongly-typed ViewModel from the DataContext object.</returns>
+		public static TViewModel GetViewModel<TViewModel>(this Object dataContext)
+			where TViewModel : class
+		{
+			_ = dataContext ?? throw new ArgumentNullException($"DataContext for {typeof(TViewModel)} is null");
+
+			if (dataContext is not TViewModel viewModel)
+			{
+				throw new InvalidOperationException($"Unexpected type of DataContext: Expected {typeof(TViewModel)}, actual is {dataContext.GetType()}");
+			}
+
+			return viewModel;
+		}
+	}
+}
---
+++ tests/CodeFuller.Library.Wpf.UnitTests/Extensions/ObjectExtensionsTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using CodeFuller.Library.Wpf.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeFuller.Library.Wpf.UnitTests.Extensions
+{
+	[TestClass]
+	public class ObjectExtensionsTests
+	{
+		private class ViewModel1
+		{
+		}
+
+		private class ViewModel2
+		{
+		}
+
+		[TestMethod]
+		public void GetViewModel_IfDataContextHasCorrectType_ReturnsStronglyTypedViewModel()
+		{
+			// Arrange
+
+			var target = new ViewModel1();
+
+			// Act
+
+			var result = target.GetViewModel<ViewModel1>();
+
+			// Assert
+
+			Assert.AreSame(target, result);
+		}
+
+		[TestMethod]
+		public void GetViewModel_IfDataContextHasIncorrectType_ThrowsInvalidOperationException()
+		{
+			// Arrange
+
+			var target = new ViewModel1();
+
+			// Act
+
+			ViewModel2 Call() => target.GetViewModel<ViewModel2>();
+
+			// Assert
+
+			var exception = Assert.ThrowsException<InvalidOperationException>(Call);
+			Assert.AreEqual("Unexpected type of DataContext: Expected CodeFuller.Library.Wpf.UnitTests.Extensions.ObjectExtensionsTests+ViewModel2, actual is CodeFuller.Library.Wpf.UnitTests.Extensions.ObjectExtensionsTests+ViewModel1", exception.Message);
+		}
+
+		[TestMethod]
+		public void GetViewModel_IfDataContextIsNull_ThrowsArgumentNullException()
+		{
+			// Arrange
+
+			ViewModel1 target = null;
+
+			// Act
+
+			ViewModel1 Call() => target.GetViewModel<ViewModel1>();
+
+			// Assert
+
+			Assert.ThrowsException<ArgumentNullException>(Call);
+		}
+	}
+}
